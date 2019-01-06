8e71b1ad6c8c4bb18f2aee3bdba7bcd6425da72b
jdfrozen <616769416@qq.com>
2019-01-06 17:02:28 +0800

    add spring cloud

---
+++ spring-cloud-client/src/main/java/com/frozen/springcloudclient/SpringcloudclientApplication.java
@@ -0,0 +1,14 @@
+package com.frozen.springcloudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudclientApplication.class, args);
+    }
+
+}
+

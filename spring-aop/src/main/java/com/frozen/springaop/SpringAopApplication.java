9cf2b1e26cd0015f13a1e80af60f20101c7f0bde
jdfrozen <616769416@qq.com>
2019-01-16 09:36:45 +0800

    新增spring aop的应用

--- spring-aop/src/main/java/com/frozen/springaop/SpringAopApplication.java
+++ spring-aop/src/main/java/com/frozen/springaop/SpringAopApplication.java
@@ -1,12 +1,14 @@
-package com.frozen.learn;
+package com.frozen.springaop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnApplication {
+public class SpringAopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearnApplication.class, args);
+        SpringApplication.run(SpringAopApplication.class, args);
     }
+
 }
+
--- spring-aop/src/test/java/com/frozen/springaop/SpringAopApplicationTests.java
+++ spring-aop/src/test/java/com/frozen/springaop/SpringAopApplicationTests.java
@@ -1,4 +1,4 @@
-package com.frozen.learn;
+package com.frozen.springaop;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LearnApplicationTests {
+public class SpringAopApplicationTests {
 
     @Test
     public void contextLoads() {
     }
 
 }
+

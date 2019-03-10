6b74c48f6803f1bf28e14075020e4cc19cacf0df
jdfrozen <616769416@qq.com>
2019-03-10 21:17:30 +0800

    新增springcloud客户端

---
+++ spring-cloud-client/src/main/java/com/frozen/springcloudclient/SpringCloudClientApplication.java
@@ -0,0 +1,15 @@
+package com.frozen.springcloudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudClientApplication.class, args);
+    }
+
+}

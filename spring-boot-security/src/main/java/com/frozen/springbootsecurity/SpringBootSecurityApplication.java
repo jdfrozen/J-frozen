acbb5e6fd8180c9d16232440a9903349e95d64e8
jdfrozen <616769416@qq.com>
2019-09-20 18:22:39 +0800

    springsecurity

--- spring-boot-security/src/main/java/com/frozen/springbootsecurity/SpringBootSecurityApplication.java
+++ spring-boot-security/src/main/java/com/frozen/springbootsecurity/SpringBootSecurityApplication.java
@@ -2,12 +2,20 @@ package com.frozen.springbootsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class SpringBootSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSecurityApplication.class, args);
     }
-
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }

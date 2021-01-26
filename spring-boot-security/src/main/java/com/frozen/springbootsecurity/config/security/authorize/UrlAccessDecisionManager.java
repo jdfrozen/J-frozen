5258b2b171dfaecd54921e5b6c314414e69cfafa
jiangjiudong <jiangjiudong@58.com>
2021-01-26 16:46:09 +0800

    AOP

--- spring-boot-security/src/main/java/com/frozen/springbootsecurity/config/security/authorize/UrlAccessDecisionManager.java
+++ spring-boot-security/src/main/java/com/frozen/springbootsecurity/config/security/authorize/UrlAccessDecisionManager.java
@@ -3,6 +3,8 @@ package com.frozen.springbootsecurity.config.security.authorize;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -30,13 +32,13 @@ public class UrlAccessDecisionManager implements AccessDecisionManager {
             ConfigAttribute ca = iterator.next();
             //当前请求需要的权限
             String needRole = ca.getAttribute();
-//            if ("ROLE_LOGIN".equals(needRole)) {
-//                if (auth instanceof AnonymousAuthenticationToken) {
-//                    throw new BadCredentialsException("未登录");
-//                } else {
-//                    return;
-//                }
-//            }
+            if ("ROLE_LOGIN".equals(needRole)) {
+                if (auth instanceof AnonymousAuthenticationToken) {
+                    throw new BadCredentialsException("未登录");
+                } else {
+                    return;
+                }
+            }
             //当前用户所具有的权限
             Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
             for (GrantedAuthority authority : authorities) {
